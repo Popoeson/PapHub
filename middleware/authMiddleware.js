c6ab0169a503577462ff01bb78bff80133de0817
Samuel Popoola <samuelpopoola323@gmail.com>
2026-04-26 13:57:01 +0100

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,15 @@
-yo
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  const token = req.header('Authorization');
+
+  if (!token) return res.status(401).json({ msg: 'No token' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Invalid token' });
+  }
+};
